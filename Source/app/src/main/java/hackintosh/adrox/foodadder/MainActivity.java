a955fa76276c87872cbb113fd8e697eba27998b5
adrixop95 <adrixop95@me.com>
2018-09-05 23:46:45 +0200

    scaling and cleanup

--- Source/app/src/main/java/hackintosh/adrox/foodadder/MainActivity.java
+++ Source/app/src/main/java/hackintosh/adrox/foodadder/MainActivity.java
@@ -4,48 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-    Button scancode_button;
     Button add_button;
     Button history_button;
-
-    IntentResult result;
-
-    public String[] history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scancode_button = (Button) findViewById(R.id.home_scan);
         add_button = (Button) findViewById(R.id.home_add);
         history_button = (Button) findViewById(R.id.home_history);
 
-        scancode_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v){
-                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-                integrator.setPrompt("Zbliż skaner do kodu produktu");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.initiateScan();
-            }
-        });
 
         add_button.setOnClickListener(new View.OnClickListener() {
 
@@ -67,24 +39,6 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null){
-            if(result.getContents()==null){
-                Toast.makeText(this, "Anulowałeś skanowanie", Toast.LENGTH_LONG).show();
-            }
-            else {
-                Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
-
-
-            }
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
 
 }
--- Source/app/src/main/java/hackintosh/adrox/foodadder/history.java
+++ Source/app/src/main/java/hackintosh/adrox/foodadder/history.java
@@ -1,21 +1,78 @@
 package hackintosh.adrox.foodadder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class history extends AppCompatActivity {
+
+public class history extends ListActivity {
+    Button scancode_button;
+    IntentResult result;
+    ListView listview;
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_history);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        setListAdapter(adapter);
+
+        scancode_button = (Button) findViewById(R.id.addBtn);
+        listview = (ListView) findViewById(android.R.id.list);
+
+        scancode_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                IntentIntegrator integrator = new IntentIntegrator(history.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Zbliż skaner do kodu produktu");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Anulowałeś skanowanie", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+
+                listItems.add(result.getContents());
+                adapter.notifyDataSetChanged();
+
+                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(history.this, "Clicked at" + position, Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
